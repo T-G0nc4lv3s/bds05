3d95439c8b57618724751379e1d13a5b565af134
Thiago Gonçalves <mg.az.ul@hotmail.com>
2022-08-26 21:39:51 -0300

    Insert new User

--- src/main/java/com/devsuperior/movieflix/controllers/UserController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/UserController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.dto.UserInsertDTO;
 import com.devsuperior.movieflix.services.UserService;
 
 @RestController
@@ -22,4 +25,12 @@ public class UserController {
 		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok(dto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO insertDTO){
+		UserDTO dto = new UserDTO();
+		dto = service.insert(insertDTO);
+		return ResponseEntity.ok(dto);
+	}
+
 }
--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.RoleDTO;
 import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.dto.UserInsertDTO;
+import com.devsuperior.movieflix.entities.Role;
 import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.RoleRepository;
 import com.devsuperior.movieflix.repositories.UserRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
@@ -17,10 +21,35 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserInsertDTO dto) {
+		User user = new User();
+		dtoToEntity(dto, user);
+		user = repository.save(user);
+		return new UserDTO(user);	
+	}
+
+	
+	private void dtoToEntity(UserInsertDTO dto, User user) {
+		
+		user.setName(dto.getName());
+		user.setEmail(dto.getEmail());
+		user.setPassword(dto.getPassword());
+		user.getRoles().clear();
+		
+		for(RoleDTO roleDTO : dto.getRoles()) {
+			Role role = roleRepository.getOne(roleDTO.getId());
+			user.getRoles().add(role);
+		}
 	}
 }
